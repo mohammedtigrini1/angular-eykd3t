992d0b24f89ee6cf521e5618ccc8ca85037473b0
mohammedtigrini1 <mohammed.tigrini@appdirect.com>
2022-07-25 23:04:18 -0400

    1 file modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,43 @@ export class AppComponent {
   // its important myCanvas matches the variable name in the template
   @ViewChild('myCanvas')
   myCanvas: ElementRef<HTMLCanvasElement>;
-
   public context: CanvasRenderingContext2D;
+
+  public totalTime = 20000;
+  public step = 10;
 
   ngAfterViewInit(): void {
     this.context = this.myCanvas.nativeElement.getContext('2d');
-    this.context.fillStyle = '#FF0000';
-    this.context.fillRect(0, 0, 150, 75);
+    this.animationLoop();
   }
+
+  animationLoop() {
+    let temp = 0;
+    const interval = setInterval(() => {
+      if (temp > this.totalTime) {
+        clearInterval(interval);
+        return;
+      }
+      this.erase();
+      this.drawRectangle(temp);
+      temp += this.step;
+    }, this.step);
+  }
+
+  erase() {
+    this.context.fillStyle = '#FFFFFF';
+    this.context.fillRect(
+      0,
+      0,
+      this.myCanvas.nativeElement.width,
+      this.myCanvas.nativeElement.height
+    );
+  }
+
+  drawRectangle(time) {
+    this.context.fillStyle = '#FF0000';
+    this.context.fillRect(time / 200, time / 200, 40, 40);
+  }
+
+  appear() {}
 }
