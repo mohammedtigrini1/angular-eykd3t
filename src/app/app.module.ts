fb9ec336995204cd8d29584b57ae3f8aa7dbb913
mohammedtigrini1 <mohammed.tigrini@appdirect.com>
2022-07-31 22:01:09 -0400

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ShapesService } from './shapes.service';
+import { DrawService } from './draw.service';
 
 @NgModule({
+  providers: [ShapesService, DrawService],
   imports: [BrowserModule, FormsModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
